8317eb8eff2f9ef6f08aae175bc4d36f354b463b
Sourav Ghosh <sourav.dh.litcult@gmail.com>
2025-12-15 00:21:24 +0530

    Create Three_themes_Keywords_JSON.py

---
+++ Three_themes_Keywords_JSON.py
@@ -0,0 +1,64 @@
+KEYWORD_THEME_MAP = {
+
+    # -------------------------
+    # CONFINEMENT
+    # -------------------------
+    "nervous": "Confinement",
+    "nervousness": "Confinement",
+    "hysterical": "Confinement",
+    "condition": "Confinement",
+    "weakness": "Confinement",
+    "burden": "Confinement",
+    "effort": "Confinement",
+    "exhaust": "Confinement",
+    "tired": "Confinement",
+    "suffer": "Confinement",
+    "fancies": "Confinement",
+    "false": "Confinement",
+    "foolish": "Confinement",
+    "impressionable": "Confinement",
+    "locked": "Confinement",
+    "fastened": "Confinement",
+    "press": "Confinement",
+    "broken": "Confinement",
+
+    # -------------------------
+    # THE PATTERN (Wallpaper)
+    # -------------------------
+    
+    "grotesques": "The_Pattern",
+    "hideous": "The_Pattern",
+    "queer": "The_Pattern",
+    "haunted": "The_Pattern",
+    "smell": "The_Pattern",
+    "fungus": "The_Pattern",
+    "lurid": "The_Pattern",
+    "formless": "The_Pattern",
+    "inharmonious": "The_Pattern",
+    "puzzling": "The_Pattern",
+    "dream": "The_Pattern",
+
+    # -------------------------
+    # RUPTURE OF REPRESSIVE ORDER
+    # -------------------------
+    "creep": "Rupture_of_Repressive_Order",
+    "creepy": "Rupture_of_Repressive_Order",
+    "confusion": "Rupture_of_Repressive_Order",
+    "confuse": "Rupture_of_Repressive_Order",
+    "delirium": "Rupture_of_Repressive_Order",
+    "tremens": "Rupture_of_Repressive_Order",
+    "fatuity": "Rupture_of_Repressive_Order",
+    "suspect": "Rupture_of_Repressive_Order",
+    "inexplicable": "Rupture_of_Repressive_Order",
+    "unreliable": "Rupture_of_Repressive_Order",
+    "shakes": "Rupture_of_Repressive_Order",
+    "affected": "Rupture_of_Repressive_Order",
+    "desperate": "Rupture_of_Repressive_Order",
+    "shriek": "Rupture_of_Repressive_Order",
+    "derision": "Rupture_of_Repressive_Order",
+    "fainted": "Rupture_of_Repressive_Order",
+    "humiliating": "Rupture_of_Repressive_Order",
+    "shame": "Rupture_of_Repressive_Order",
+    "faint": "Rupture_of_Repressive_Order",
+    "Fainted": "Rupture_of_Repressive_Order"
+}
